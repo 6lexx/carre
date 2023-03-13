7ed9aeafa1b087e44e3bc1a199712d45708dce3e
Matthys Verbregue <matthys.verbregue@gmail.com>
2023-03-13 15:40:51 +0100

    Version saisie Clavier documentée

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿int calculCarre(int val)
+﻿int calculCarre(int val) 
 {
-    return val * val;
+    return val * val;  //retour du carré de la valeur rentrée
 }
 
-Console.WriteLine("Entrez un entier");
-string str = Console.ReadLine();
-int val = Convert.ToInt32(str);
-Console.WriteLine(calculCarre(val));
+Console.WriteLine("Entrez un entier");  //demande de saisie clavier
+string str = Console.ReadLine();        //saisie clavier
+int val = Convert.ToInt32(str);         //convertion de la saisie
+Console.WriteLine(calculCarre(val));    //appel de la fonction avec val de la saisie
